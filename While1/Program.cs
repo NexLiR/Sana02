131e2ea2e11546df852c53930cf1b4184031fda0
Vlad Shevchuk <ipz231_shvye@student.ztu.edu.ua>
2023-11-20 21:07:12 +0200

    Complete work with While1 task

---
+++ While1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    static void Main()
+    {
+        double a, b, c;
+        int n = 0;
+        Console.WriteLine("Enter a value for n");
+        do
+        {
+            n = int.Parse(Console.ReadLine());
+            if (n <= 0)
+            {
+                Console.WriteLine("n must be a positive number");
+                continue;
+            }
+        } while (n <= 0);
+        a = expression1(n);
+        b = expression2(n);
+        c = expression3(n);
+        Console.WriteLine("a = -1/3 + 1/5 + ... + (-1)^n/(2*n+1) = {0}", a);
+        Console.WriteLine("b = (1+1/1^2) + (1+1/2^2) + ... + (1+1/n^2) = {0}", b);
+        Console.WriteLine("c = 1! + 2! + n! = {0}", c);
+    }
+
+    static double expression1(int n)
+    {
+        double sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += Math.Pow(-1, n) / (2 * n + 1);
+        }
+        return sum;
+    }
+    static double expression2(int n)
+    {
+        double sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += 1 + 1 / Math.Pow(n, 2);
+        }
+        return sum;
+    }
+    static double expression3(int n)
+    {
+        double sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += calcFactorial(i);
+        }
+        return sum;
+    }
+
+    static int calcFactorial(int i) 
+    {
+        if (i == 0 || i == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return i * calcFactorial(i - 1);
+        }
+    }
+}

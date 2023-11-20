cd1f9a8f1917d30f7c77765d095b36254e85857f
Vlad Shevchuk <ipz231_shvye@student.ztu.edu.ua>
2023-11-20 20:24:34 +0200

    Complete work with PointsAverage task

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Transactions;
+
+class Program
+{
+    static void Main()
+    {
+        double grade, sum = 0, count = 0, avg;
+        Console.WriteLine("Enter 5 student grades on a 100-point scale:");
+        do
+        {
+            grade = double.Parse(Console.ReadLine());
+            if (grade < 0 || grade > 100)
+            {
+                Console.WriteLine("Entered value is out of range");
+                continue;
+            }
+            sum += grade;
+            count++;
+        } while (count != 5);
+        avg = sum / count;
+        Console.WriteLine("Student average grade: {0}", avg);
+        if (avg >= 50) Console.WriteLine("Student is admitted to take exam");
+        else Console.WriteLine("Student is not allowed to take exam");
+    }
+}

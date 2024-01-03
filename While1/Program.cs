2d5fc3371d72c13831e7f6e2f7f0c45a4e182fd2
Vlad Shevchuk <ipz231_shvye@student.ztu.edu.ua>
2024-01-03 17:42:45 +0200

    Fix errors in While1

--- While1/Program.cs
+++ While1/Program.cs
@@ -23,7 +23,7 @@ class Program
         c = expression3(n);
         Console.WriteLine("a = -1/3 + 1/5 + ... + (-1)^n/(2*n+1) = {0}", a);
         Console.WriteLine("b = (1+1/1^2) + (1+1/2^2) + ... + (1+1/n^2) = {0}", b);
-        Console.WriteLine("c = 1! + 2! + n! = {0}", c);
+        Console.WriteLine("c = 1! + 2! + ... + n! = {0}", c);
     }
 
     static double expression1(int n)
@@ -31,7 +31,7 @@ class Program
         double sum = 0;
         for (int i = 1; i <= n; i++)
         {
-            sum += Math.Pow(-1, n) / (2 * n + 1);
+            sum += Math.Pow(-1, i) / (2 * i + 1);
         }
         return sum;
     }
@@ -40,7 +40,7 @@ class Program
         double sum = 0;
         for (int i = 1; i <= n; i++)
         {
-            sum += 1 + 1 / Math.Pow(n, 2);
+            sum += 1 + 1.0 / Math.Pow(i, 2);
         }
         return sum;
     }
@@ -56,6 +56,10 @@ class Program
 
     static int calcFactorial(int i) 
     {
+        if (i < 0)
+        { 
+            throw new Exception("Factorial of negative number is undefined");
+        }
         if (i == 0 || i == 1)
         {
             return 1;
